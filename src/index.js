04996204d175f28288d982a7b7720c267f7c58d5
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-08-30 11:15:00 +0300

    fix: bug import React

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import StarRating from "./components/StarRating";
+// import StarRating from "./components/StarRating";
 // import reportWebVitals from './reportWebVitals';
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color="purple" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating color="purple" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
