bb4141cca782bbacc9534f9d519b7e8e8bf95f5a
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-08-30 11:31:47 +0300

    fix: API URL

--- src/App.js
+++ src/App.js
@@ -301,7 +301,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
           setIsLoading(true);
           setError("");
           const response = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`,
             {
               signal: controller.signal,
             }
